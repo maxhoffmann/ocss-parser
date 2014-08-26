201c05a1c6de41c986702d97dc42837ac5c4e239
Maximilian Hoffmann <account@maximilianhoffmann.com>
2014-08-26 11:51:04 +0200

    add basic AST creation

--- index.js
+++ index.js
@@ -1,47 +1,118 @@
 module.exports = function(name, ocss) {
   validate(arguments);
 
-  var ast = object(name);
-
-  return ocss
-    .split('\n')
-    .reduce(function(context, line, linenum, rules) {
-      if (empty(line)) return context;
-
-      return context;
-    }, ast);
-
   function validate(args) {
     var name = args[0];
     var ocss = args[1];
 
     if ( ! name) throw new Error('missing object name param');
-    if ( /-/.test(name)) throw new Error('dashes are not allowed in object names');
+    if (/-/.test(name)) throw new Error('dashes are not allowed in object names');
 
     if (typeof ocss !== 'string') throw new Error('missing ocss param');
   }
 
-  function object(name) {
+  function removeComments(line) {
+    return line.replace(/#.*$/, '');
+  }
+
+  function toObjects(line, linenum) {
+    return {
+      raw: line.trim(),
+      linenum: linenum+1
+    };
+  }
+
+  function isNotEmpty(line) {
+    return ! /^\s*$/.test(line.raw);
+  }
+
+  function addIndentation(line) {
+    line.indentation = line.raw.match(/^\s*/)[0].length;
+    line.raw = line.raw.replace(/^\s+/, '');
+    return line;
+  }
+
+  function addType(line) {
+    line.type = type(line.raw, line.linenum);
+    return line;
+  }
+
+  function type(string, linenum) {
+    if (isElement(string)) return 'element';
+    if (isDeclaration(string)) return 'declaration';
+
+    throw new Error('Syntax error on line '+linenum+': '+string);
+  }
+
+  function isElement(string) {
+    return /^[A-z]+$/.test(string);
+  }
+
+  function isDeclaration(string) {
+    return /^\s*([A-z-]+):\s*([A-z\-]+)\s*$/.test(string);
+  }
+
+  function parseType(line) {
+    return parse[line.type](line);
+  }
+
+  function toAST(context, line, index, lines) {
+    if (index === lines.length-1) return line;
+    var ast = lines[lines.length-1];
+    if (!line.indentation) {
+      context = ast;
+      if (line.type === 'declaration') context = ast.elements[0];
+    }
+    context = context[line.type+'s'];
+    delete line.indentation;
+    delete line.linenum;
+    delete line.raw;
+    context.push(line);
+    return context;
+  }
+
+  var parse = {};
+
+  parse.object = function(name) {
     return {
       type: 'object',
       name: name,
       variables: [],
-      elements: [],
-      modifiers: [],
+      elements: [parse.element({raw: name})],
+      modifiers: [],
       parentModifiers: []
     };
-  }
+  };
 
-  function empty(string) {
-    return !/^\s*$/.test(string);
-  }
+  parse.element = function(line) {
+    line.type = line.type || 'element';
+    line.name = line.raw;
+    line.declarations = [];
+    line.elements = [];
+    delete line.raw;
 
-  function comment(string) {
-    return !/^#/.test(string);
-  }
+    return line;
+  };
 
-  function notIndented(string) {
-    return !/^\s/.test(string);
-  }
+  parse.declaration = function(line) {
+    var values = line.raw.match(/^(.+):\s*(.+)$/);
 
+    line.property = values[1];
+    line.value = values[2];
+
+    return line;
+  };
+
+  var object = parse.object(name);
+
+  return ocss
+    .split('\n')
+    .map(removeComments)
+    .map(toObjects)
+    .filter(isNotEmpty)
+    .map(addIndentation)
+    .map(addType)
+    .map(parseType)
+    .concat(object)
+    .reduce(toAST, object);
 };
--- test/index.js
+++ test/index.js
@@ -16,28 +16,138 @@ test('throws errors using invalid params', function(is) {
   }, 'throws without ocss');
 });
 
+test('comments', function(is) {
+  is.plan(1);
+
+  var actual = parse('test', '# Comment 1\ndisplay: block # Comment 2\n\t#asdas');
+  var expected = {
+    type: 'object',
+    name: 'test',
+    elements: [{
+      type: 'element',
+      name: 'test',
+      declarations: [{
+        type: 'declaration',
+        property: 'display',
+        value: 'block'
+      }],
+      elements: []
+    }],
+    modifiers: [],
+    parentModifiers: [],
+    variables: []
+  };
+  is.same(actual, expected, 'skips comments');
+});
+
 test('AST', function(is) {
-  is.plan(2);
+  is.plan(5);
 
-  var ast = parse('test', '');
-  var desired = {
+  var actual = parse('test', '');
+  var expected = {
     type: 'object',
     name: 'test',
-    elements: [],
+    elements: [{
+      type: 'element',
+      name: 'test',
+      declarations: [],
+      elements: []
+    }],
     modifiers: [],
     parentModifiers: [],
     variables: []
   };
-  is.same(ast, desired, 'empty AST for empty string');
+  is.same(actual, expected, 'empty AST for empty string');
 
-  ast = parse('test', '\n');
-  desired = {
+  actual = parse('test', '\n');
+  expected = {
+      type: 'object',
+      name: 'test',
+      elements: [{
+        type: 'element',
+        name: 'test',
+        declarations: [],
+        elements: []
+      }],
+      modifiers: [],
+      parentModifiers: [],
+      variables: []
+    };
+  is.same(actual, expected, 'empty AST for empty lines');
+
+  actual = parse('test', 'display: block');
+  expected = {
     type: 'object',
     name: 'test',
-    elements: [],
+    elements: [{
+      type: 'element',
+      name: 'test',
+      declarations: [{
+        type: 'declaration',
+        property: 'display',
+        value: 'block'
+      }],
+      elements: []
+    }],
     modifiers: [],
     parentModifiers: [],
     variables: []
   };
-  is.same(ast, desired, 'empty AST for empty lines');
+  is.same(actual, expected, 'adds element with declaration');
+
+  actual = parse('test', 'display: block\ncolor: red');
+  expected = {
+    type: 'object',
+    name: 'test',
+    elements: [{
+      type: 'element',
+      name: 'test',
+      declarations: [{
+        type: 'declaration',
+        property: 'display',
+        value: 'block'
+      },
+      {
+        type: 'declaration',
+        property: 'color',
+        value: 'red'
+      }],
+      elements: []
+    }],
+    modifiers: [],
+    parentModifiers: [],
+    variables: []
+  };
+  is.same(actual, expected, 'adds element with declarations');
+
+  actual = parse('test', 'display: block\ncolor: red\nchild');
+  expected = {
+    type: 'object',
+    name: 'test',
+    elements: [{
+      type: 'element',
+      name: 'test',
+      declarations: [{
+        type: 'declaration',
+        property: 'display',
+        value: 'block'
+      },
+      {
+        type: 'declaration',
+        property: 'color',
+        value: 'red'
+      }],
+      elements: []
+    },
+    {
+      type: 'element',
+      name: 'child',
+      declarations: [],
+      elements: []
+    }],
+    modifiers: [],
+    parentModifiers: [],
+    variables: []
+  };
+  is.same(actual, expected, 'adds element without declaration');
 });
