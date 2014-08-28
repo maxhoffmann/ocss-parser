f43fb7fc1ef2da42360e24efdff71002ab0ec609
Maximilian Hoffmann <account@maximilianhoffmann.com>
2014-08-28 22:52:54 +0200

    remove console log from test js

--- test/index.js
+++ test/index.js
@@ -53,10 +53,6 @@ cases.forEach(function(name) {
     var actual = parse('test', readFile(source));
     var expected = JSON.parse(readFile(ast));
 
-    if (name === 'complex example') {
-      console.log(JSON.stringify(actual, null, 2), '\n---\n', JSON.stringify(expected, null, 2));
-    }
-
     is.same(actual, expected, name);
     is.end();
   });
