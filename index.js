6bf1a90ea3bb6ebf78a606c3df9b2bfca77e9b3b
jonykrause <buero@jonathan-krause.de>
2014-08-29 14:57:39 +0200

    add minimal liveedit

---
+++ index.js
@@ -0,0 +1,14 @@
+var t;
+var o = require('observable');
+var h = require('hyperscript');
+var ocss = require('ocss-parser');
+
+document.body.appendChild(
+  h('div',
+    t = h('textarea.in', '', { autofocus: true }),
+    h('pre.out', o.transform(o.input(t), function(txt) {
+      try { return JSON.stringify(ocss('OCSS_REPL', txt), null, 2) }
+      catch(e) { return e }
+    }))
+  )
+)
