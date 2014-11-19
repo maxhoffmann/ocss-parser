ae7494f7ed8ca693a2aedaeb4c990dde14c763d1
Maximilian Hoffmann <account@maximilianhoffmann.com>
2014-11-19 22:38:55 +0100

    remove semicolons

--- test/index.js
+++ test/index.js
@@ -1,66 +1,66 @@
 "use strict";
-var test = require('tape');
-var fs   = require('fs');
-var path = require('path');
+var test = require('tape')
+var fs   = require('fs')
+var path = require('path')
 
-var parse = require('../').parse;
-var stringify = require('../').stringify;
+var parse = require('../').parse
+var stringify = require('../').stringify
 
 test('params', function(is) {
-  is.plan(4);
+  is.plan(4)
 
-  is.throws(parse, 'throws without params');
+  is.throws(parse, 'throws without params')
   is.throws(function() {
-    parse('');
-  }, 'throws without object name');
+    parse('')
+  }, 'throws without object name')
   is.throws(function() {
-    parse('invalid-name');
-  }, 'throws if object name is invalid');
+    parse('invalid-name')
+  }, 'throws if object name is invalid')
   is.throws(function() {
-    parse('test');
-  }, 'throws without ocss');
-});
+    parse('test')
+  }, 'throws without ocss')
+})
 
 test('errors', function(is) {
   is.throws(function() {
-    parse('test', 'element\n\t\tdisplay: block');
-  }, 'too much indentation');
+    parse('test', 'element\n\t\tdisplay: block')
+  }, 'too much indentation')
 
   is.throws(function() {
-    parse('test', 'display: block\n\tcolor: red');
-  }, 'indentation after declaration');
+    parse('test', 'display: block\n\tcolor: red')
+  }, 'indentation after declaration')
 
   is.throws(function() {
-    parse('test', 'element\n\t=modifier');
-  }, 'nested modifier');
+    parse('test', 'element\n\t=modifier')
+  }, 'nested modifier')
 
-  is.end();
-});
+  is.end()
+})
 
-var cases = fs.readdirSync(path.join(__dirname, 'cases'));
+var cases = fs.readdirSync(path.join(__dirname, 'cases'))
 
 cases.forEach(function(name) {
 
   test(name, function(is) {
-    var dir    = path.join(__dirname, 'cases', name);
-    var source = path.join(dir, 'test.ocss');
-    var ast    = path.join(dir, 'ast.json');
-    var css    = path.join(dir, 'test.css');
+    var dir    = path.join(__dirname, 'cases', name)
+    var source = path.join(dir, 'test.ocss')
+    var ast    = path.join(dir, 'ast.json')
+    var css    = path.join(dir, 'test.css')
 
-    var actualAST   = parse('test', readFile(source));
-    var expectedAST = JSON.parse(readFile(ast));
-    is.same(actualAST, expectedAST, name);
+    var actualAST   = parse('test', readFile(source))
+    var expectedAST = JSON.parse(readFile(ast))
+    is.same(actualAST, expectedAST, name)
 
-    var actualCSS   = stringify(actualAST);
-    var expectedCSS = readFile(css);
-    is.equal(actualCSS, expectedCSS, name);
-    is.end();
-  });
+    var actualCSS   = stringify(actualAST)
+    var expectedCSS = readFile(css)
+    is.equal(actualCSS, expectedCSS, name)
+    is.end()
+  })
 
-});
+})
 
 function readFile(file) {
-  var src = fs.readFileSync(file, 'utf8');
-  src = src.replace(/\r\n/, '\n'); // normalize line endings
-  return src;
+  var src = fs.readFileSync(file, 'utf8')
+  src = src.replace(/\r\n/, '\n') // normalize line endings
+  return src
 }
